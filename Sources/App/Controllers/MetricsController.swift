b6308d7e0930f4047a6d919240daa61e70383701
Oleh Hudeichuk <emptystamp@gmail.com>
2020-06-29 22:16:43 +0300

    clean

--- Sources/App/Controllers/MetricsController.swift
+++ Sources/App/Controllers/MetricsController.swift
@@ -18,7 +18,6 @@ final class MetricsController {
 
     func index(_ req: Request) throws -> EventLoopFuture<String> {
         let prometheusClient: PrometheusClient = try! MetricsSystem.prometheus()
-        setTimeDiffMetric(prometheusClient)
 
         getDiskLoad { (metric) in
             self.setDiskMetric(metric)
@@ -38,15 +37,6 @@ final class MetricsController {
         prometheusClient.collect(promise.succeed)
 
         return promise.futureResult
-    }
-
-    
-    private func setTimeDiffMetric(_ prom: PrometheusClient) {
-        let timeDiff = getTimeDiff()
-        let gauge = prom.createGauge(forType: Int.self, named: "TimeDiff")
-//        gauge.inc() // Increment by 1
-//        gauge.dec(19) // Decrement by given value
-        gauge.set(timeDiff)
     }
 
     private func setDiskMetric(_ metric: DiskLoad) {
@@ -89,19 +79,6 @@ final class MetricsController {
 
 
     // MARK: Collector Helpers
-    private func getTimeDiff() -> Int {
-        guard let scriptDir = Environment.get("ScriptDir") else { return 0 }
-        let command = "cd \(scriptDir)/ && ./check_node_sync_status.sh"
-        if let out = try? systemCommand(command),
-            let maybeTimeDiff = out.regexp(#"TIME_DIFF.+(-*\d+)"#)[1],
-            let timeDiff = Int(maybeTimeDiff)
-        {
-            return timeDiff
-        }
-
-        return 0
-    }
-
     private func getDiskSpace() -> Int {
         guard let scriptDir = Environment.get("ScriptDir") else { return 0 }
         let command = "cd \(scriptDir)/ && ./check_node_sync_status.sh"
